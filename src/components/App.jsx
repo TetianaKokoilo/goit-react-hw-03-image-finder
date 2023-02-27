49cf4d2bd9989e4337fa6060fb5c5faba1cb3de6
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-27 15:57:57 +0200

    add Serachbar jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
+import Searchbar from '../components/Searchbar/Searchbar';
+
 export const App = () => {
   return (
     <div>
-      {/* <Searchbar /> */}
+      <Searchbar />
       {/* <ImageGallery /> */}
       {/* <Button/> */}
     </div>
