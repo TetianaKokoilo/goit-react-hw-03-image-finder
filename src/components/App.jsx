02245ff8561d52212b0ee021f7b27f8353884f54
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-02-28 18:00:41 +0200

    add PixApi

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
-import { ImageGallery } from './ImageGallery/ImageGallery'
+import { ImageGallery } from './ImageGallery/ImageGallery';
 
-const API_KEY = '32970758-8ba6ee6d9fec7577e22e4216e';
+// const API_KEY = '32970758-8ba6ee6d9fec7577e22e4216e';
 export class App extends Component {
   state = {
     searchImage: '',
@@ -10,15 +10,15 @@ export class App extends Component {
     isLoading: false,
   };
 
-  componentDidMount() {
-    this.setState({ isLoading: true });
-    fetch(
-      `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
-    )
-      .then(response => response.json())
-      .then(image => this.setState({ image }))
-      .finally(() => this.setState({ isLoading: false }));
-  }
+  // componentDidUpdate(prevProps, prevState) {
+  //   this.setState({ isLoading: true });
+  //   fetch(
+  //     `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+  //   )
+  //     .then(response => response.json())
+  //     .then(image => this.setState({ image }))
+  //     .finally(() => this.setState({ isLoading: false }));
+  // }
 
   getSearchSubmit = searchImage => {
     this.setState({ searchImage });
